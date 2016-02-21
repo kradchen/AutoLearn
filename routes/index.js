4b649d9b3025510c3957cc9c6a181b7b770933e5
KradChen <87714763@qq.com>
2016-02-21 19:03:40 +0800

    。。。

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,57 @@ var router = express.Router();
 var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    beginLearn();
   res.render('index', { title: 'Express' });
 });
 function beginLearn() {
-  //login
-  request.post('http://yuhang.learning.gov.cn/system/login.php', {form: {username: '', passwd: '', loginType: '1'}},
-      function () {
-        //getlist
-        request.get('http://yuhang.learning.gov.cn/study/index.php?act=studycourselist',
-            function (error, response, body) {
-              //分析body，获取CourseId
-              var courseIdAry = new Array();
-              //getLogId
-              request.post('http://yuhang.learning.gov.cn/study/ajax.php', {
-                    form: {
-                      act: 'insert',
-                      courseId: courseIdAry[0]
-                    }
-                  },delayUpdate(body,courseIdAry));
+    //login
+    console.log("try login!");
+    request.post('http://yuhang.learning.gov.cn/system/login.php', {form: {username: '330106198412035215', passwd: 'flychj123', loginType: '1'}},
+        function (error,response,body) {
+            console.log(body);
+            console.log("try get list!");
+            //getlist
+            request.get('http://yuhang.learning.gov.cn/study/index.php?act=studycourselist',
+                function (oerror,oresponse,obody) {
+                    var bodyString =oresponse.body.innerHTML;
+                    console.log(bodyString);
+                    //分析body，获取CourseId
+                    var FirstCourseId = bodyString.substring(bodyString.indexOf("act=detail&courseid=\"") + 21, 10);
+                    //getLogId
+                    console.log("try get logid!");
+                    request.post('http://yuhang.learning.gov.cn/study/ajax.php', {
+                        form: {
+                            act: 'insert',
+                            courseId: FirstCourseId
+                        }
+                    }, function (ierror, iresponse, body) {
+                        var logId = body.logId;
+                        console.log("logid:"+logId);
+                        //updateTime
+                        request.post('http://yuhang.learning.gov.cn/study/ajax.php', function (iierror, iiresponse, ibody) {
+                                //完成
+                                if (ibody.err == '1') {
+console.log("error=1");
+                                }
+                                //未完成
+                                if (ibody.err == '0') {
+                                    console.log("error=0");
+                                }
+                            },
+                            {
+                                form: {
+                                    act: 'update',
+                                    courseId: FirstCourseId,
+                                    logId: logId
+                                }
+                            });
+                    });
 
-                  });
-            });
-  //function ()
-};
+                });
+        });
+    //function ()
+}
 function delayUpdate(sbody,courseIdAry) {
   var logId = sbody.logId;
   //updateTime
